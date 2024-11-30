40378059f6ba42a76125637a81ad1ffacd448b25
Jose Ruben Mariño Garcia <ruben.marinogarcia@plexus.es>
2024-11-30 18:30:45 +0100

    Scaling horizontally

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
+const { availableParallelism } = require('node:os');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
+
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    // create one worker per available core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            PORT: 3000 + i
+        });
+    }
+
+    // set up the adapter on the primary thread
+    return setupPrimary();
+}
 
 async function main() {
     // open the database file
@@ -24,7 +40,9 @@ async function main() {
     const app = express();
     const server = createServer(app);
     const io = new Server(server, {
-        connectionStateRecovery: {}
+        connectionStateRecovery: {},
+        // set up the adapter on each worker thread
+        adapter: createAdapter()
     });
 
     app.get('/', (req, res) => {
@@ -64,8 +82,11 @@ async function main() {
         }
     });
 
-    server.listen(3000, () => {
-        console.log('server running at http://localhost:3000');
+    // each worker will listen on a distinct port
+    const port = process.env.PORT;
+
+    server.listen(port, () => {
+        console.log(`server running at http://localhost:${port}`);
     });
 }
 
